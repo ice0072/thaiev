71d32903f23053aacb14f240661d847617c197f0
newtratip <tharathip.chaweewongphan@gmail.com>
2022-10-06 12:15:17 +0700

    Add module

---
+++ odoo/custom/src/private/thaiev_install/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ThaiEV: Install",
+    "summary": "Used to install other modules",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Thai EV",
+    "author": "Ecosoft",
+    "installable": True,
+    "depends": [
+        "contacts",
+        "sale_management",
+        "sale_stock",
+        "purchase_stock",
+        "account",
+        "mis_builder",
+        "l10n_th_partner",
+        "l10n_th_account_asset_management",
+        "account_asset_compute_batch",
+    ],
+}
