12e8309265f48d3e67551c14f95c8e4a330dbf02
newtratip <tharathip.chaweewongphan@gmail.com>
2022-12-13 00:18:44 +0700

    [ADD] thaiev_config

---
+++ odoo/custom/src/private/thaiev_config/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "ThaiEV: Config",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Thai EV",
+    "author": "Ecosoft",
+    "installable": True,
+    "depends": [
+        "purchase_request",
+        "purchase",
+    ],
+    "data": [
+        "data/ir_actions_server_data.xml",
+        "data/tier_definition_data.xml",
+    ]
+}
