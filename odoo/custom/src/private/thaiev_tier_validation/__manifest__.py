320a98b9c8c1060c2186b2cf567d04708ba5b6e2
Saran440 <saranl@ecosoft.co.th>
2022-12-27 12:28:30 +0700

    [ADD] thatev_tier_validation

---
+++ odoo/custom/src/private/thaiev_tier_validation/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "ThaiEV: Tier Validation",
+    "summary": "Function common with tier validation ThaiEV project",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "NxPO",
+    "author": "Ecosoft",
+    "depends": ["base_tier_validation"],
+    "data": [],
+    "maintainers": ["Saran440"],
+    "development_status": "Alpha",
+}
---
+++ odoo/custom/src/private/thaiev_tier_validation/models/account_payment.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super()._get_under_validation_exceptions()
+        res.extend(["tax_invoice_ids", "wht_move_ids"])
+        return res
